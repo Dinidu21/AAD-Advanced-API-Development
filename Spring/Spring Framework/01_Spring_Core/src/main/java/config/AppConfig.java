0e12d6e1286cd945c23e716622398c482a67cc1c
Dinidu21 <dinidusachintha3@gmail.com>
2025-06-14 21:23:22 +0530

    basic setup

--- Spring/Spring Framework/01_Spring_Core/src/main/java/config/AppConfig.java
+++ Spring/Spring Framework/01_Spring_Core/src/main/java/config/AppConfig.java
@@ -1,10 +1,12 @@
 package config;
 
+import bean.SpringBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan (basePackages = "bean")
+@ComponentScan (basePackageClasses = SpringBean.class)
 public class AppConfig {
 
 }
