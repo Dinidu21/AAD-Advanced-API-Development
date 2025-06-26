73d8386c12d735da290dc50ca05d571d69405827
Dinidu21 <dinidusachintha3@gmail.com>
2025-06-26 13:16:08 +0530

    Spring System Env

---
+++ Spring/Spring Framework/07_Spring_Env/src/main/java/com/dinidu/bean/Bean.java
@@ -0,0 +1,10 @@
+package com.dinidu.bean;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Bean {
+    public Bean() {
+        System.out.println("Bean constructor called");
+    }
+}
