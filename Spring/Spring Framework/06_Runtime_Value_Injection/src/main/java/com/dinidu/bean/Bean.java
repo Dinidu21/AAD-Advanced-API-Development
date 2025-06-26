5de7fa40173a335a79b947e4d20aedc8e692280b
Dinidu21 <dinidusachintha3@gmail.com>
2025-06-26 11:38:15 +0530

    Refactor Bean class constructor to simplify parameters and enhance logging

--- Spring/Spring Framework/06_Runtime_Value_Injection/src/main/java/com/dinidu/bean/Bean.java
+++ Spring/Spring Framework/06_Runtime_Value_Injection/src/main/java/com/dinidu/bean/Bean.java
@@ -1,31 +1,30 @@
 package com.dinidu.bean;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Bean {
+
+    public Bean(
+            @Value("Dinidu") String name,
+            @Value("#{systemProperties['os.name']}") String osName
+    ) {
+        System.out.println(
+                "Bean constructor called with name: " + name + " \nand OS: " + osName
+        );
+    }
+
+    @Autowired
     public Bean(
             @Value("Dinidu") String name,
             @Value("#{systemProperties['os.name']}") String osName,
-            @Value("#{systemEnvironment['JAVA_HOME']}") String javaHome,
-            @Value("#{systemEnvironment['PATH']}") String path,
-            @Value("#{systemProperties['user.timezone']}") String timezone,
-            @Value("#{systemProperties['user.language']}") String language,
-            @Value("#{systemProperties['user.country']}") String country,
-            @Value("#{systemProperties['user.dir']}") String userDir,
-            @Value("#{systemProperties['user.name']}") String userName
-
+            @Value("#{systemProperties['java.version']}") String javaVersion
     ) {
-        System.out.println("Bean constructor called " + name +
-                " with OS: " + osName +
-                ", JAVA_HOME: " + javaHome +
-                ", JAVA Path: " + path +
-                ", Timezone: " + timezone +
-                ", Language: " + language +
-                ", Country: " + country +
-                ", User Directory: " + userDir +
-                ", User Name: " + userName
+        System.out.println(
+                "Bean constructor called with name: \n" + name + " \nand OS: " + osName + "\nand Java Version: " + javaVersion
         );
     }
+
 }
