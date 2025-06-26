91bdce0129948af49e973c5e5b4a6b94e89575da
Dinidu21 <dinidusachintha3@gmail.com>
2025-06-26 11:41:16 +0530

    Update Bean class constructors to handle multiple @Autowired annotations @Autowired(required = false)

--- Spring/Spring Framework/06_Runtime_Value_Injection/src/main/java/com/dinidu/bean/Bean.java
+++ Spring/Spring Framework/06_Runtime_Value_Injection/src/main/java/com/dinidu/bean/Bean.java
@@ -6,7 +6,14 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Bean {
+    // When using @Autowired, the constructor will be called automatically by Spring
+    // when there is 2 Autowired constructors, Spring will throw an exception
+    // because it cannot decide which constructor to use.
+    // To avoid this, we can use @Autowired(required = false) to make the constructor optional.
+    // if  we use more than one constructor with @Autowired with (required = false) then it will
+    // call the highest paras constructor it finds.
 
+    @Autowired(required = false)
     public Bean(
             @Value("Dinidu") String name,
             @Value("#{systemProperties['os.name']}") String osName
@@ -16,7 +23,7 @@ public class Bean {
         );
     }
 
-    @Autowired
+    @Autowired(required = false)
     public Bean(
             @Value("Dinidu") String name,
             @Value("#{systemProperties['os.name']}") String osName,
