af89a8dde384371bce486361c1cff1c57967673c
Dinidu21 <dinidusachintha3@gmail.com>
2025-06-26 14:45:00 +0530

    Add Spring MVC configuration and initial controller setup

---
+++ Spring/Spring Framework/08_Spring_Web_MVC/src/main/java/com/dinidu/config/WebAppConfig.java
@@ -0,0 +1,11 @@
+package com.dinidu.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@ComponentScan (basePackages = {"com.dinidu.bean","com.dinidu.controller"})
+@EnableWebMvc
+public class WebAppConfig {
+}
---
+++ Spring/Spring Framework/08_Spring_Web_MVC/src/main/java/com/dinidu/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.dinidu.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("hello")
+public class HelloController {
+    public HelloController() {
+        System.out.println("HelloController initialized");
+    }
+
+    @GetMapping
+    public String hello() {
+        return "Hello, Spring MVC!";
+    }
+
+}
