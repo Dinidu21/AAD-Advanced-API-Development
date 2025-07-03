076a5e7dfa2525c96352dd835bc609820785cba5
Dinidu21 <dinidusachintha3@gmail.com>
2025-07-03 11:25:42 +0530

    PathVariable

---
+++ Spring/Spring Framework/REST_Controllers/src/main/java/com/dinidu/controller/PathVariableController.java
@@ -0,0 +1,22 @@
+package com.dinidu.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/path")
+public class PathVariableController {
+
+    @GetMapping("/{id}")
+    public String getPathVariable(@PathVariable("id") String id) {
+        return "Path Variable ID: " + id;
+    }
+
+    @GetMapping("/{name}/{age}")
+    public String getPathVariableWithMultipleParams(@PathVariable("name") String name,
+                                                    @PathVariable("age") int age) {
+        return "Name: " + name + ", Age: " + age;
+    }
+}
